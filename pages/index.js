5ec0541406f0bf1ebbdff01ae598bffd80f03400
yobe <yogebaa@gmail.com>
2023-04-23 09:01:30 +0530

    added chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Head from "next/head";
+// pages/index.js
+import {
+  Box,
+  VStack,
+  Heading,
+  Text,
+  FormControl,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import styles from "./index.module.css";
+import Nav from "../components/Nav";
 
 export default function Home() {
   const [healthInput, setHealthInput] = useState("");
   const [result, setResult] = useState();
   const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -31,52 +42,52 @@ export default function Home() {
         results = JSON.parse(data.result);
       }
 
-      const elements = results.map((item, index) => {
-        return (
-          <div key={index}>
-            <h4>{item.Question}</h4>
-            <p>{item.Answer}</p>
-          </div>
-        );
-      });
+      const elements = results.map((item, index) => (
+        <Box key={index}>
+          <Heading size="md" marginBottom="2">
+            {item.Question}
+          </Heading>
+          <Text>{item.Answer}</Text>
+        </Box>
+      ));
 
       setResult(elements);
       setHealthInput("");
     } catch (error) {
       console.error(error);
-      alert(error.message);
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
     }
     setLoading(false);
   }
 
   return (
-    <div>
-      <Head>
-        <title>Supplement Search</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Search my Supplement</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="health"
-            placeholder="Enter an health condition, symptom, disease or outcome"
-            value={healthInput}
-            onChange={(e) => setHealthInput(e.target.value)}
-          />
-          <input type="submit" value="Generate" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-      <a
-        target="_blank"
-        href="https://www.amazon.in/dp/B01DQV8BIM?ref=nb_sb_ss_w_as-reorder-t1_k0_1_5&amp;amp=&amp;crid=3OWGXHNXRNVI3&amp;sprefix=ashwa&amp;th=1&_encoding=UTF8&tag=examine0b-21&linkCode=ur2&linkId=2bf50aa798fc8c91cd1effa0fb7ddd3d&camp=3638&creative=24630"
-      >
-        ash
-      </a>
-    </div>
+    <VStack spacing="4" align="center">
+      <Nav />
+      <Heading as="h3" size="lg">
+        Self-evaluate your optimal health needs
+      </Heading>
+      <FormControl as="form" width="100%" maxWidth="40em" onSubmit={onSubmit}>
+        <Input
+          type="text"
+          name="health"
+          placeholder="Enter a health condition, symptom, disease, or outcome"
+          value={healthInput}
+          onChange={(e) => setHealthInput(e.target.value)}
+          marginBottom="4"
+        />
+        <Button type="submit" isLoading={loading} colorScheme="blue">
+          Generate
+        </Button>
+      </FormControl>
+      <VStack spacing="6" align="start" width="100%" maxWidth="40em">
+        {result}
+      </VStack>
+    </VStack>
   );
 }
