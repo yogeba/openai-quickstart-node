2adbf1d48abe5a57b00c7ee48c6f396f51f19b5a
yobe <yogebaa@gmail.com>
2023-04-27 11:12:23 +0530

    Updated streaming true

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,18 +1,7 @@
 // pages/api/generate.js
 import { fetchHealthData, generatePrompt } from "../../lib/openai";
 
-// export default async function (req, res) {
-//   try {
-//     const health = req.body.health || "";
-//     const result = await fetchHealthData(health);
-//     return res.json({ result });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(400).json({ error: { message: error.message } });
-//   }
-// }
-
-import { OpenAIStream } from "../../utils/OpenAIStream";
+import { OpenAIStream } from "../../lib/OpenAIStream";
 
 export const config = {
   runtime: "edge",
